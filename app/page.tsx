32a485cbddb1ae0c9dd97e55d95aeeac926729b6
Guilherme Honorio <guileandro006@gmail.com>
2025-02-28 13:54:20 -0300

    Criando section de video e princing

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import { Header } from "./components/landingPage/header";
 import { Hero } from "./components/landingPage/hero";
+import { Pricing } from "./components/landingPage/pricing";
+import { VideoExplanation } from "./components/landingPage/video-explanation";
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto">
       <Header />
       <Hero />
+      <VideoExplanation />
+      <Pricing />
     </div>
   );
 }
