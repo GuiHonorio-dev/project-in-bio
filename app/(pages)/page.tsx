3214a7c52b4af9ed702578378fac3e1bb6238c76
Guilherme Honorio <guileandro006@gmail.com>
2025-02-28 19:32:55 -0300

    Criacao da pagina de criar link

---
+++ app/(pages)/page.tsx
@@ -0,0 +1,17 @@
+import { FAQ } from "../components/landingPage/faq";
+import { Header } from "../components/landingPage/header";
+import { Hero } from "../components/landingPage/hero";
+import { Pricing } from "../components/landingPage/pricing";
+import { VideoExplanation } from "../components/landingPage/video-explanation";
+
+export default function Home() {
+  return (
+    <div className="max-w-7xl mx-auto">
+      <Header />
+      <Hero />
+      <VideoExplanation />
+      <Pricing />
+      <FAQ />
+    </div>
+  );
+}
