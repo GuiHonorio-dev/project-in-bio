6e86522664778f60a1469b4c1d61a2ae91dc3a76
Guilherme Leandro <guileandro006@gmail.com>
2025-03-24 10:12:06 -0300

    Finalizacao do user-card e ajeitando alguns erros

--- app/(pages)/[profileId]/page.tsx
+++ app/(pages)/[profileId]/page.tsx
@@ -14,6 +14,7 @@ export default async function ProfilePage({ params }: { params: Promise<{ profil
   const { profileId } = await params
   
   const profileData = await getProfileData(profileId)
+  console.log(`Profile Data: ${profileData}`)
 
   if (!profileData) return notFound()
 
@@ -21,6 +22,7 @@ export default async function ProfilePage({ params }: { params: Promise<{ profil
 
   const projects = await getProfileProjects(profileId)
   
+  console.log(`Projects: ${projects}`)
   return (
     <div className="relative h-screen flex p-20 overflow-hidden">
       <div className="fixed top-0 left-0 w-full flex justify-center items-center gap-1 py-2 bg-background-tertiary">
--- app/components/commons/project-card.tsx
+++ app/components/commons/project-card.tsx
@@ -1,44 +1,54 @@
-'use client'
+"use client";
 
-import type { ProjectData } from '@/app/server/get-profile-data'
-import Image from 'next/image'
-import Link from 'next/link'
+import { formatUrl } from "@/app/lib/utils";
+import { ProjectData } from "@/app/server/get-profile-data";
+import Link from "next/link";
+import { useParams } from "next/navigation";
 
-interface ProjectCardProps {
-  project: ProjectData
-  isOwner: boolean
-  img: string
-}
+export function ProjectCard({
+  project,
+  isOwner,
+  img,
+  name,
+  description,
+}: {
+  project?: ProjectData;
+  isOwner?: boolean;
+  img: string;
+  name?: string;
+  description?: string;
+}) {
+  const { profileId } = useParams();
+  const formattedUrl = formatUrl(project?.projectUrl || "https://projectinbio.com");
 
+  async function handleClick() {
+    if (!profileId || !project?.id || isOwner) return;
 
-export function ProjectCard({ project, img, isOwner }: { project: ProjectData, isOwner: boolean, img: string }) {
-  
-  const projectUrl = project.projectUrl
-  const formattedUrl = projectUrl.startsWith("http") ? projectUrl : `https://${projectUrl}`
-  
-  function handleClick() {
-    console.log("clicked") // TODO: Implementar analytics
   }
-  
+
   return (
-    <Link href={formattedUrl} target='_blank' onClick={handleClick}>
+    <Link href={formattedUrl} target="_blank" onClick={handleClick}>
       <div className="w-[340px] h-[132px] flex gap-5 bg-background-secondary p-3 rounded-[20px] border border-transparent hover:border-border-secondary">
-        <div className="size-24 rounded-md overflow-hidden shrink-0">
-          <img src={img} alt="Imagem do projeto" className="w-full h-full object-cover" />
+        <div className="size-24 rounded-md overflow-hidden flex-shrink-0">
+          <img src={img} alt="Projeto" className="w-full h-full object-cover" />
         </div>
         <div className="flex flex-col gap-2">
-          {
-            isOwner && (
+          {isOwner && (
             <span className="uppercase text-xs font-bold text-accent-green">
-              {project.totalVisits || 0 } cliques
-            </span>  
-            )
-          }
+              {project?.totalVisits || 0} cliques
+            </span>
+          )}
+
           <div className="flex flex-col">
-            <span className="text-white font-bold">{project.projectName}</span>
-            <span className="text-content-body text-sm">{project.projectDescription}</span>
+            <span className="text-white font-bold">
+              {name || project?.projectName}
+            </span>
+            <span className="text-content-body text-sm">
+              {description || project?.projectDescription}
+            </span>
           </div>
         </div>
       </div>
     </Link>
-  )}
+  );
+}
