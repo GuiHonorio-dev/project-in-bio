f9e23dd3d488cab094b8c4277c93457af8368c27
Guilherme Honorio <guileandro006@gmail.com>
2025-05-18 19:37:35 -0300

    Criando o componente header

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { Header } from "./components/landingpage/header";
 import { Hero } from "./components/landingpage/hero";
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto">
+      <Header />
       <Hero />
     </div>
   );
