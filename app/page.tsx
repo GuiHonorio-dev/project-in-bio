348c27979b74e680bcd4af42cb39e0098003462b
Guilherme Honorio <guileandro006@gmail.com>
2025-05-18 19:11:44 -0300

    Criacao hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Hero } from "./components/landingpage/hero";
+
 export default function Home() {
   return (
-    <div className="h-screen bg-background-primary">
-      <p></p>
+    <div className="max-w-7xl mx-auto">
+      <Hero />
     </div>
   );
 }
