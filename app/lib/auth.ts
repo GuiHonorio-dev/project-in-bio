9e30576ffa3c0d62338ed0459177a45b474aa152
Guilherme Honorio <guileandro006@gmail.com>
2025-05-29 12:14:41 -0300

    Configuracao da autenticacao com o google

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -3,9 +3,9 @@ import NextAuth from "next-auth";
 import { firebaseCert } from "./firebase";
 import Google from "next-auth/providers/google";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const { handlers, signIn, signOut, auth  } = NextAuth({
   adapter: FirestoreAdapter({
     credential: firebaseCert,
-  }),
+  }), 
   providers: [Google]
 })
