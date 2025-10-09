bb4dc2e24bdbf44a71696c288501f44117b9bb53
Guilherme Honorio <guileandro006@gmail.com>
2025-10-09 20:36:35 -0300

    Criacao do componente de UserCard

--- app/components/ui/text-area.tsx
+++ app/components/ui/text-area.tsx
@@ -5,8 +5,6 @@ export function TextArea(props: React.TextareaHTMLAttributes<HTMLTextAreaElement
     <textarea {...props} className={cn(`
       w-full p-3 bg-background-secondary text-white placeholder:text-placeholder rounded-xl 
       border border-transparent hover:border-borderSecondary hover:text-content-body active:border-borderTertiary
-
-
       `, props.className)} />
   )
 }
