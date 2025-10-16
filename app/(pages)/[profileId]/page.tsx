c3393843ae767123a5813b2a49f1d2e717fb85c1
Guilherme Honorio <guileandro006@gmail.com>
2025-10-16 14:36:12 -0300

    criacao do modal

--- app/(pages)/[profileId]/page.tsx
+++ app/(pages)/[profileId]/page.tsx
@@ -1,11 +1,23 @@
 import { ProjectCard } from "@/app/components/commons/projec-card"
 import { TotalVisits } from "@/app/components/commons/total-visits"
 import { UserCard } from "@/app/components/commons/user-card"
-import { Plus } from "lucide-react"
+import { auth } from "@/app/lib/auth"
+import { getProfileData } from "@/app/server/get-profile-data"
 import Link from "next/link"
+import { notFound } from "next/navigation"
+import { NewProject } from "./new-project"
 
 export default async function ProfilePage({ params }: { params: Promise<{ profileId: string }> }) {
   const { profileId } = await params
+  const session = await auth()
+
+
+  const profileData = await getProfileData(profileId)
+
+  if(!profileData) return notFound()
+
+  const isOwner = profileData.userId === session?.user?.id
+
   return (
     <div className="relative h-screen flex p-20 overflow-hidden">
       <div className="fixed top-0 left-0 w-full flex justify-center items-center gap-1 py-2 bg-background-tertiary">
@@ -25,10 +37,11 @@ export default async function ProfilePage({ params }: { params: Promise<{ profil
         <ProjectCard />
         <ProjectCard />
         <ProjectCard />
-        <button className="w-[400px] h-[155px] rounded-[20px] bg-background-secondary flex items-center gap-2 justify-center hover:border border-dashed">
-          <Plus className="size-10 text-accent-green"/>
-          <span>Novo projeto</span>
-        </button>
+
+        {
+          isOwner && <NewProject profileId={profileId} />
+        }
+        
       </div>
       <div className="absolute bottom-4 right-0 left-0 w-min mx-auto">
         <TotalVisits />
