a4ce4a4eeb2f5c55bdd4ec5915c48d3964594339
Guilherme Honorio <guileandro006@gmail.com>
2025-10-16 13:31:49 -0300

    Criacao da funcionalidade de criar link

---
+++ app/(pages)/criar/create-link-form.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import { createLink } from "@/app/actions/create-link";
+import { verifyLink } from "@/app/actions/verify-link";
+import { Button } from "@/app/components/ui/button";
+import { TextInput } from "@/app/components/ui/text-input";
+import { sanitizeLink } from "@/app/lib/utils";
+import { useRouter } from "next/navigation";
+import { useState, type ChangeEvent, type FormEvent } from "react";
+
+export function CreateLinkForm() {
+  const router = useRouter()
+
+  const [link, setLink] = useState("")
+  const [error, setError] = useState("")
+
+  function handleLinkChange(e: ChangeEvent<HTMLInputElement>) {
+    setLink(sanitizeLink(e.target.value))
+    setError('')
+  }
+
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    if(link.length === 0) return setError("Escolha um link primeiro :)")
+    
+    const isLinkTaken = await verifyLink(link)
+    
+    if(isLinkTaken) return setError('Desculpe, esse link ja esta em uso :/')
+
+    const isLinkCreated = await createLink(link)
+  
+    if(!isLinkCreated) return setError("Erro ao criar perfil. Tente novamente")
+  
+    router.push(`/${link}`)
+    }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="w-full flex items-center gap-2">
+        <span className="text-white">projectinbio.com/</span>
+        <TextInput 
+          value={link} 
+          onChange={handleLinkChange} 
+        />
+        <Button className="w-[126px]">Criar</Button>
+      </form>
+      <div>
+        <span className="text-accent-pink">{error}</span>
+      </div>
+    </>
+  )
+}
